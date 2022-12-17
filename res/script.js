ca8ef3d858c1ff5a55c9a5415cf14fc10a41e710
Markus <65797058+headquarter8302@users.noreply.github.com>
2022-12-17 07:33:53 -0400

    buncha updates

---
+++ res/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const countElem = document.getElementById("articleCount");
+fetch(
+  "https://youtube.fandom.com/api.php?action=query&format=json&meta=siteinfo&formatversion=latest&siprop=statistics"
+)
+  .then((response) => response.json())
+  .then(data => countElem.textContent = new Intl.NumberFormat().format(data.query.statistics.articles));
+  
