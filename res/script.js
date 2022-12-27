6a9c996b3f3313928bcec2b645d16a16bd6ee134
Markus <65797058+headquarter8302@users.noreply.github.com>
2022-12-27 08:07:45 -0400

    fix ogp descriptors and cors

--- res/script.js
+++ res/script.js
@@ -2,8 +2,8 @@
 
 const countElem = document.getElementById("articleCount");
 fetch(
-  "https://youtube.fandom.com/api.php?action=query&format=json&meta=siteinfo&formatversion=latest&siprop=statistics"
+  "https://youtube.fandom.com/api.php?action=query&format=json&meta=siteinfo&formatversion=latest&siprop=statistics",
+  {mode: 'cors'}
 )
   .then((response) => response.json())
   .then(data => countElem.textContent = new Intl.NumberFormat().format(data.query.statistics.articles));
-  
