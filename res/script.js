175db477a5e62389fc4527c1a077df5510fca5c6
Eterhox <74748724+Eterhox@users.noreply.github.com>
2023-07-21 06:02:48 -0500

    this should replace the .yml file (#12)
    * this should replace the .yml file
    
    stalked some websites to get this working - this should be able to replace the .yml file
    
    * convert to IIFE
    
    ---------
    
    Co-authored-by: Markus <65797058+headquarter8302@users.noreply.github.com>

--- res/script.js
+++ res/script.js
@@ -1,8 +1,13 @@
-"use strict";
+(function() {
+  const a = 'https://youtube.fandom.com/api.php?action=query&format=json&meta=siteinfo&formatversion=latest&siprop=statistics';
+  const sc = document.createElement('script');
 
-const countElem = document.getElementById("articleCount");
-fetch("count.json", { mode: "no-cors" })
-  .then((response) => response.json())
-  .then(
-    (data) => (countElem.textContent = new Intl.NumberFormat().format(data.int))
-  );
+  window.handler = function(data) {
+    const ac = data.query.statistics.articles;
+    const htmlSwitch = document.getElementById('articleCount');
+    htmlSwitch.textContent = ac.toLocaleString();
+  };
+
+  sc.src = a + '&callback=handler';
+  document.body.appendChild(sc);
+})();
